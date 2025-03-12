eb983a3e1978daf432a0a83ab75f5a2eb870e3fd
ntl-anees <muhammad.anees@nayatel.com>
2025-03-12 15:22:15 +0500

    Final commit

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const cors = require('cors');
 
 const app = express();
 
+
 // setting the origin as our frontend or request origin
 var corsOptions={
     origin: "http://localhost:8081"
 };
+
 
 app.use(cors(corsOptions));
 
@@ -14,10 +16,28 @@ app.use(express.json()); // to parse requests of type json
 
 app.use(express.urlencoded({extended: true}));
 
-// a simple route
-app.get("/",(req,res) =>{
-    res.json({message:"Welcome to Sequelizer Rest APIs"})
-});
+
+
+// syncing sequelize
+const db = require("./app/models");
+
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+  
+
+// // a simple route
+// app.get("/",(req,res) =>{
+//     res.json({message:"Welcome to Sequelizer Rest APIs"})
+// });
+
+require("./app/routes/tutorial.routes")(app);
+
 
 // setting port to run server on
 const PORT = process.env.PORT || 8080;
